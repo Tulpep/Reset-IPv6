7c5ff0042576aaf06443c50a03ca6ca3344ed491
Ricardo Polo <ricardo.polo@tulpep.com>
2014-07-27 16:06:18 -0500

    Now showing progress status

--- RS_ResetIPv6.ps1
+++ RS_ResetIPv6.ps1
@@ -40,6 +40,8 @@ end {}
 
 }
 
+Write-DiagProgress -Activity "Analyzing the system"
+
 $arch =  Get-OSArchitecture -ComputerName localhost
 if($arch.Architecture -like "64-Bit")
 {
@@ -50,18 +52,35 @@ else
  Copy-Item .\devcon32.exe C:\Windows\System32\devcon.exe
 }
 
+Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapater 6to4"
 devcon.exe remove *6to4mp
+Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapater Teredo"
 devcon.exe remove *teredo
+Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapter IP-HTTPS"
 devcon.exe remove *IPHTTPS
+Write-DiagProgress -Activity "Uninstalling IPv6 interfaces" -Status "Tunnel adapter Isatap"
 devcon.exe remove *ISATAP
 
+
 Set-Location $env:WINDIR\inf
+Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter 6to4"
 devcon.exe install nettun.inf *6to4mp
+Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter Teredo"
 devcon.exe install nettun.inf *teredo
+Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter IP-HTTPS"
 devcon.exe install nettun.inf *IPHTTPS
+Write-DiagProgress -Activity "Installing IPv6 drivers"  -Status "Tunnel adapter Isatap"
 devcon.exe install nettun.inf *ISATAP
 
+Write-DiagProgress -Activity "Resetting IPv6 settings" -Status "Netsh interface ipv6 reset"
 netsh interface ipv6 reset
+Write-DiagProgress -Activity "Updating group policies" -Status "GPUpdate /Force"
 gpupdate /force
+
+$input = Get-DiagInput -Id "WantToRestart"
+if($input -eq $true)
+{
+Restart-Computer
+} 
 
 Return 0
