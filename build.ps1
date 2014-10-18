90f23008170d34501ced2f7886dc6f11d93d5eb7
Ricardo Polo <ricardo.polo@tulpep.com>
2014-10-18 18:51:50 -0500

    Showing progress in build script

--- build.ps1
+++ build.ps1
@@ -1,11 +1,14 @@
 $resourcesPath = ".\build-resources"
 $buildFolder = ".\build-folder"
 
+
+Write-Host Deleting old build folder
 If (Test-Path $buildFolder){
 	Remove-Item $buildFolder -Recurse
 }
 New-Item -Path $buildFolder -ItemType directory
 
+Write-Host Copying files to build folder
 Copy-Item ResetIpv6.cdf $buildFolder
 Copy-Item ResetIPv6.diagpkg $buildFolder
 Copy-Item RS_ResetIPv6.ps1 $buildFolder
@@ -14,6 +17,7 @@ Copy-Item VF_ResetIPv6.ps1 $buildFolder
 Copy-Item devcon32.exe $buildFolder
 Copy-Item devcon64.exe $buildFolder
 
+Write-Host Creating Windows Catalog File
 $makeCatParamters = "-v " + $buildFolder + "\ResetIpv6.cdf"
 Start-Process -FilePath ($resourcesPath + "\MakeCat.Exe") -ArgumentList $makeCatParamters
 Remove-Item ($buildFolder + "\ResetIpv6.cdf")
